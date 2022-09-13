6bcc7499e70285ae88444740596f3e077a8d5e89
Tecno-wiz <st.andrewboye@yahoo.com>
2022-09-13 16:15:36 -0700

    Task 1 print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Descriptions: a function to print alphabet
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letters = 'a';
+
+	while (letters <= 'z')
+	{
+		_putchar(letters);
+		letters++;
+	}
+	_purchar('\n')
+}
