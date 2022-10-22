ce778b967f6b3e14951fedf2501093456ea5f0bb
Tecno-wiz <st.andrewboye@yahoo.com>
2022-10-21 19:16:09 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,6 +3,7 @@
 /**
  * add_node - adds a new node at the beginning
  * of a list_t list
+ * @head: head of the linked list
  * @str: string to store in the list
  * Return: address of the head
  */
